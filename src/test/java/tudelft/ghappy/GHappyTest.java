8014ce92e77a23c8443c5c55e32d670d837c33fc
vpavlov <lerikpav@gmail.com>
2018-11-19 14:09:38 +0300

    Happy G: Fixed error and implemented tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,48 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GHappyTest {
+    @Test
+    public void happyStartGTest() {
+        assertTrue(new GHappy().gHappy("gga"));
+    }
+
+    @Test
+    public void happyMiddleGTest() {
+        assertTrue(new GHappy().gHappy("agga"));
+    }
+
+    @Test
+    public void happyEndGTest() {
+        assertTrue(new GHappy().gHappy("agg"));
+    }
+
+    @Test
+    public void happyOnlyGTest() {
+        assertTrue(new GHappy().gHappy("gg"));
+    }
+
+    @Test
+    public void nonHappyGStartTest() {
+        assertFalse(new GHappy().gHappy("gaga"));
+    }
+
+    @Test
+    public void nonHappyGMiddleTest() {
+        assertFalse(new GHappy().gHappy("agaga"));
+    }
+
+    @Test
+    public void nonHappyGEndTest() {
+        assertFalse(new GHappy().gHappy("agag"));
+    }
+
+    @Test
+    public void happyAndNonHappyGTest() {
+        assertFalse(new GHappy().gHappy("aggag"));
+    }
 }
