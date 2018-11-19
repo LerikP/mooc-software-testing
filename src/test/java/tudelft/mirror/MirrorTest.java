f37b0fbac9b596e5ccb434b3e2ade7b004a7d423
vpavlov <lerikpav@gmail.com>
2018-11-19 14:02:30 +0300

    Mirror: Fixed error and implemented tests

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,22 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MirrorTest {
+    @Test
+    public void isEvenMirror() {
+        assertEquals("abba", new Mirror().mirrorEnds("abba"));
+    }
+
+    @Test
+    public void isOddMirror() {
+        assertEquals("abcba", new Mirror().mirrorEnds("abcba"));
+    }
+
+    @Test
+    public void isNotMirror() {
+        assertEquals("ab", new Mirror().mirrorEnds("abcdba"));
+    }
 }
