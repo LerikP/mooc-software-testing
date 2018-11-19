27ccb42687b9cb8adf4c13c625acf74d09456cce
vpavlov <lerikpav@gmail.com>
2018-11-19 14:24:24 +0300

    CaesarShiftCipher: Fixed error and implemented tests

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,43 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CaesarShiftCipherTest {
+    @Test
+    public void simpleShiftTest(){
+        assertEquals("b", new CaesarShiftCipher().CaesarShiftCipher("a", 1));
+    }
+
+    @Test
+    public void wrappingBackwardsShiftTest(){
+        assertEquals("z", new CaesarShiftCipher().CaesarShiftCipher("a", -1));
+    }
+
+    @Test
+    public void wrappingForwardsShiftTest(){
+        assertEquals("a", new CaesarShiftCipher().CaesarShiftCipher("z", 1));
+    }
+
+    @Test
+    public void allCharactersTest(){
+        assertEquals("bcdefghijklmnopqrstuvwxyza", new CaesarShiftCipher().CaesarShiftCipher("abcdefghijklmnopqrstuvwxyz", 1));
+    }
+
+    @Test
+    public void modShiftTest(){
+        assertEquals("b", new CaesarShiftCipher().CaesarShiftCipher("a", 27));
+    }
+
+    @Test
+    public void modShiftBackwardsTest(){
+        assertEquals("a", new CaesarShiftCipher().CaesarShiftCipher("b", -27));
+    }
+
+
+    @Test
+    public void invalidInputTest(){
+        assertEquals("invalid", new CaesarShiftCipher().CaesarShiftCipher("1", 1));
+    }
 }
